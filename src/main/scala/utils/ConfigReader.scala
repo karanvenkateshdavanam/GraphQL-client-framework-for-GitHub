b35eb95de70ad7ed110ae49844ef73818e1fbb60
Karan Venkatesh Davanam <kdavan2@uic.edu>
2020-09-08 19:40:42 -0500

    adding config reader object to read values

---
+++ src/main/scala/utils/ConfigReader.scala
@@ -0,0 +1,17 @@
+package utils
+import com.typesafe.config._
+
+/**
+ *Single instance which provides a method to read configuration file details
+ */
+object ConfigReader {
+  /**
+   *
+   * @param configFileName config file name
+   * @return static instance of Config
+   */
+  def getConfigDetails(configFileName:String):Config={
+    ConfigFactory.load(configFileName)
+  }
+
+}
